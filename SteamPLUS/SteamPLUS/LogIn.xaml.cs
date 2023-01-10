fe17d686102fe1b9124d6ac2f39ff82d66187de8
Gio <94830910+PS226715@users.noreply.github.com>
2023-01-10 09:41:37 +0100

    Test push
    Test

---
+++ SteamPLUS/SteamPLUS/LogIn.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SteamPLUS
+{
+    /// <summary>
+    /// Interaction logic for LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                // Perform the desired action
+            }
+        }
+    }
+}
--- SteamPLUS/SteamPLUS/MainWindow.xaml.cs
+++ SteamPLUS/SteamPLUS/MainWindow.xaml.cs
@@ -1,28 +1,52 @@
-﻿using System;
+﻿using SteamKit2;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SteamPLUS
+namespace SteamAPIExample
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    class Program
     {
-        public MainWindow()
+        static void Main(string[] args)
         {
-            InitializeComponent();
+            // Create a new SteamClient object
+            var client = new SteamClient();
+
+            // Connect to Steam
+            client.Connect();
+
+            // Create a new SteamUser object
+            var user = client.GetHandler<SteamUser>();
+
+            // Log in to Steam using your username and password
+            user.LogOn(new SteamUser.LogOnDetails
+            {
+                Username = "your_username",
+                Password = "your_password"
+            });
+
+            // Wait for the logon process to complete
+            while (user.LoggedOn == false)
+            {
+                Task.Delay(1000).Wait();
+            }
+
+            // Create a new SteamApps object
+            var apps = client.GetHandler<SteamApps>();
+
+            // Request a list of all the games in the user's library
+            var games = apps.GetAppList().Result;
+
+            // Save the list of games to a local file
+            using (var writer = new StreamWriter("games.txt"))
+            {
+                foreach (var game in games)
+                {
+                    writer.WriteLine(game.Name);
+                }
+            }
         }
     }
 }
